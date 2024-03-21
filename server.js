5dc265936c3bdf5e80c32deba77b4f0615efe97f
rayhanhafa <115576133+rayhanhafa@users.noreply.github.com>
2024-03-21 22:57:24 +0700

    first commit

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 require('dotenv/config') //panggil env
 const mongoose = require('mongoose')
 const app = require("./app");
@@ -10,48 +11,8 @@ mongoose
   .connect(DB, {
     useNewUrlParser: true,
   }).then(con => {
-    console.log('conncetion to database succes')
+    console.log('conncetion to database success')
     //console.log(con.connections)
-  })
-
-const customerSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Name cannot be empty']
-  },
-  email: {
-    type: String,
-    unique: true,
-  },
-  phoneNumber: {
-    type: String,
-    required: true,
-  },
-  city: String,
-  country: {
-    type: String,
-    required: true,
-    default: 'Indonesia',
-  },
-
-})
-
-const Customer = mongoose.model('Customer', customerSchema)
-
-const customerTest = new Customer({
-  name: 'Ronaldo',
-  email: 'Ronaldo@gmail.com',
-  phoneNumber: '88872828282828',
-
-})
-
-customerTest
-  .save()
-  .then(doc => {
-    console.log(doc)
-  })
-  .catch((err) => {
-    console.log("ERROR : " + err)
   })
 
 app.listen(PORT, () => {
